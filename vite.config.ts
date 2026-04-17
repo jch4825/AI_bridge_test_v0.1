6a4e42ba94c825f8aee417527a47a57f06212eb7
jch4825 <jch4825@gmail.com>
2026-04-17 23:11:33 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-import react from '@vitejs/plugin-react' // React 사용 시
+import react from '@vitejs/plugin-react' 
 
 export default defineConfig(({mode}) => {
   plugins: [react()],
-  base: '/AI_bridge_test_v0.1/', // 이 부분이 핵심입니다!
+  base: '/AI_bridge_test_v0.1/', 
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
